293f2fd6a38d4d6d3a75730e413b1626f98bed43
eNcev <nestorlcevallosc@gmail.com>
2024-07-14 16:36:40 -0500

    Algoritmo burbuja 2

--- ordenamientoAlgoritmos.c
+++ ordenamientoAlgoritmos.c
@@ -24,22 +24,6 @@ void ordenamiento_burbuja(int arr[], int n)
     }
 }
 
-void ordenamiento_insercion(int arr[], int n)
-{
-    for(int i = 1; i < n; i++)
-    {
-        int valor_actual = arr[i];
-        int j = i - 1;
-        while(j >= 0 && arr[j] > valor_actual)
-        {
-            printf("Comparando %d con %d \n", arr[j], valor_actual);
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = valor_actual;
-    }
-}
-
 int main()
 {
     srand(time(NULL));
