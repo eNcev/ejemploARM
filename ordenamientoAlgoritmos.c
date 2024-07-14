8b64c9e94658dd2cf9fe4e0014903b2b62ea4360
eNcev <nestorlcevallosc@gmail.com>
2024-07-14 16:31:01 -0500

    Algoritmo burbuja

--- ordenamientoAlgoritmos.c
+++ ordenamientoAlgoritmos.c
@@ -2,6 +2,44 @@
 #include <time.h>
 #include <stdlib.h>
 
+void intercambiar(int* a, int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void ordenamiento_burbuja(int arr[], int n)
+{
+    for(int i = 0; i < n-1;  i++)
+    {
+        for(int j = 0; j < n-i-1; j++)
+        {
+            printf("Comparando %d con %d \n", arr[j], arr[j + 1]);
+            if(arr[j] > arr[j + 1])
+            {
+                intercambiar(&arr[j], &arr[j+1]);
+            }
+        }
+    }
+}
+
+void ordenamiento_insercion(int arr[], int n)
+{
+    for(int i = 1; i < n; i++)
+    {
+        int valor_actual = arr[i];
+        int j = i - 1;
+        while(j >= 0 && arr[j] > valor_actual)
+        {
+            printf("Comparando %d con %d \n", arr[j], valor_actual);
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = valor_actual;
+    }
+}
+
 int main()
 {
     srand(time(NULL));
