fde9fea1a4221b2915719dd61578c42f11966aa4
eNcev <nestorlcevallosc@gmail.com>
2024-07-14 16:48:18 -0500

    Algoritmo de insercion

--- ordenamientoAlgoritmos.c
+++ ordenamientoAlgoritmos.c
@@ -2,6 +2,22 @@
 #include <time.h>
 #include <stdlib.h>
 
+void ordenamiento_insercion(int arr[], int n)
+{
+    for(int i = 1; i < n; i++)
+    {
+        int valor_actual = arr[i];
+        int j = i - 1;
+        while(j >= 0 && arr[j] > valor_actual)
+        {
+            printf("Comparando %d con %d \n", arr[j], valor_actual);
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = valor_actual;
+    }
+}
+
 int main()
 {
     srand(time(NULL));
